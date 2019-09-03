26e7b6b883164788fb5cbcf73cf2d9475d2f3d42
unknown <2125509@labinfo.is.escuelaing.edu.co>
2019-09-03 15:56:23 -0500

    intercalado

--- BLUEPRINTS-PART1/src/main/java/edu/eci/arsw/blueprints/filter/impl/SubsamplingFiltering.java
+++ BLUEPRINTS-PART1/src/main/java/edu/eci/arsw/blueprints/filter/impl/SubsamplingFiltering.java
@@ -24,6 +24,44 @@ public class SubsamplingFiltering implements Filter {
 
     @Override
     public Set<Blueprint> filtrar(Set<Blueprint> set) {
-        return null;
+        System.out.println(set);
+        Set<Blueprint> retornar = new HashSet<>();
+
+        Iterator<Blueprint> it = set.iterator();
+        Blueprint temp;
+        int rango = 1;
+        while (it.hasNext()) {
+            List<Point> lista = new ArrayList<Point>();
+            boolean f = true;
+            temp = it.next();
+            lista.add(temp.getPoints().get(0));
+            for (int i = 1; i < temp.getPoints().size(); i++) {
+                f = true;
+                int x1 = temp.getPoints().get(i).getX();
+                int y1 = temp.getPoints().get(i).getY();
+                for (int j = 0; j < lista.size() && f; j++) {
+                    int x2 = lista.get(j).getX();
+                    int y2 = lista.get(j).getY();
+                    if (( (x1+rango == x2 && y1 == y2)|| (x1-rango == x2 && y1 == y2) 
+                            || (x1 == x2 && y1+rango == y2)) || (x1 == x2 && y1-rango == y2)
+                            || (x1+rango == x2 && y1+rango == y2) || (x1-rango == x2 && y1-rango == y2)
+                            || (x1+rango == x2 && y1-rango == y2) || (x1-rango == x2 && y1+rango == y2)
+                            ) {
+                        f = false;
+                    }
+                }
+                if (f) {
+                    lista.add(temp.getPoints().get(i));
+                }
+            }   
+            temp.setPoints(lista);
+            retornar.add(temp);
+        }
+        return retornar;
     }
 }
+
+
+
+
+
--- BLUEPRINTS-PART1/src/main/java/edu/eci/arsw/blueprints/services/App.java
+++ BLUEPRINTS-PART1/src/main/java/edu/eci/arsw/blueprints/services/App.java
@@ -22,7 +22,7 @@ public class App {
         ApplicationContext aplicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
         BlueprintsServices bluePrintS = aplicationContext.getBean(BlueprintsServices.class);
         
-        Point[] pts0 = new Point[]{new Point(40, 30), new Point(30,40),new Point(40,30),new Point(40,80),new Point(40,100),new Point(100,40)};
+        Point[] pts0 = new Point[]{new Point(40, 30), new Point(40,30),new Point(40,80),new Point(40,100),new Point(40,100)};
         Blueprint bp0 = new Blueprint("mack", "mypaint", pts0);
         Point[] pts1 = new Point[]{new Point(20, 15), new Point(40, 40)};
         Blueprint bp1 = new Blueprint("john", "mypaintJ", pts1);
@@ -34,7 +34,7 @@ public class App {
         Point[] pts5 = new Point[]{new Point(40, 40), new Point(15, 15)};
         Blueprint bp5 = new Blueprint("luis", "mypaintFrits", pts5);
         bluePrintS.addNewBlueprint(bp0);
-        bluePrintS.addNewBlueprint(bp1);
+        /*bluePrintS.addNewBlueprint(bp1);
         bluePrintS.addNewBlueprint(bp2);
         bluePrintS.addNewBlueprint(bp3);
         bluePrintS.addNewBlueprint(bp4);
@@ -42,7 +42,7 @@ public class App {
         System.out.println(bluePrintS.getBlueprint("luis", "mypaintFrits").toString());
         System.out.println(bluePrintS.getBlueprintsByAuthor("mack").toString());
         System.out.println(bluePrintS.getBlueprintsByAuthor("john").toString());
-        System.out.println(bluePrintS.getAllBlueprints().toString());
+        System.out.println(bluePrintS.getAllBlueprints().toString());*/
         for(Blueprint s: bluePrintS.getAllBlueprints()){
             System.out.println("--------------------------------");
             for (Point p: s.getPoints()){
